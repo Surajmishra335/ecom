ed5768cbf85fd40d1f7a46c3f79db1369cd8172f
suraj <surajmishra335@gmail.com>
2021-07-29 14:16:21 +0530

    product crud system done

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -92,6 +92,12 @@
                                         </li>
 
                                         <li class="menu-item">
+                                            <a title="Admin Products"
+                                                href="{{route('admin.products')}}"> All 
+                                                Products</a>
+                                        </li>
+
+                                        <li class="menu-item">
                                             <a href="{{ route('logout')}}"
                                                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                                         </li>
