7d2175c864ef85879872dd2c4ce2b60d40788b1d
suraj <surajmishra335@gmail.com>
2021-08-02 18:21:37 +0530

    home page sale funcation done

--- app/Http/Livewire/HomeComponenet.php
+++ app/Http/Livewire/HomeComponenet.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\HomeCategory;
 use App\Models\HomeSlider;
 use App\Models\Product;
+use App\Models\Sale;
 use Livewire\Component;
 
 class HomeComponenet extends Component
@@ -24,6 +25,10 @@ class HomeComponenet extends Component
 
         $no_of_products = $category->no_of_products;
 
+        $sproducts = Product::where('sale_price', '>', 0)->inRandomOrder()->get()->take(8);
+
+        $sale = Sale::find(1);
+
 
 
         return view('livewire.home-componenet', [
@@ -34,7 +39,11 @@ class HomeComponenet extends Component
 
             'categories' => $categories,
 
-            'no_of_products' => $no_of_products
+            'no_of_products' => $no_of_products,
+
+            'sproducts' => $sproducts,
+
+            'sale' => $sale,
         ])->layout('layouts.base');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Livewire\Admin\AdminEditProductComponent;
 use App\Http\Livewire\Admin\AdminHomeCategoryComponent;
 use App\Http\Livewire\Admin\AdminHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
+use App\Http\Livewire\Admin\AdminSaleComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
@@ -95,5 +96,7 @@ Route::middleware('auth:sanctum', 'verified', 'authadmin')->group(function () {
 
     Route::get('/admin/home-categories', AdminHomeCategoryComponent::class)->name('admin.homecategories');
 
+    /* Sale */
 
+    Route::get('/admin/sale', AdminSaleComponent::class)->name('admin.sale');
 });
