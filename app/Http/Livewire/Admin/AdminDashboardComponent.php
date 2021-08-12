d559c7733942bca3e7169050b44871105f049ab0
suraj <surajmishra335@gmail.com>
2021-08-12 16:40:56 +0530

    Admin Show data in main page

--- app/Http/Livewire/Admin/AdminDashboardComponent.php
+++ app/Http/Livewire/Admin/AdminDashboardComponent.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Order;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class AdminDashboardComponent extends Component
 {
     public function render()
     {
+        $orders = Order::orderBy('created_at', 'DESC')->get()->take(10);
+
+        $totalSales = Order::where('status', 'delivered')->count();
+
+        $totalRevenue = Order::where('status', 'delivered')->sum('total');
+
+        $todaySale = Order::where('status', 'delivered')->whereDate('created_at', Carbon::today())->count();
+
+        $todayRevenue = Order::where('status', 'delivered')->whereDate('created_at', Carbon::today())->sum('total');
+
         //return view('livewire.admin.admin-dashboard-component')->layout('layouts.base');
-        return view('livewire.admin.admin-dashboard-component')->layout('layouts.admin');
+        return view('livewire.admin.admin-dashboard-component', [
+
+            'orders' => $orders,
+            'totalSales' => $totalSales,
+            'totalRevenue' => $totalRevenue,
+            'todaySale' => $todaySale,
+            'todayRevenue' => $todayRevenue,
+
+        ])->layout('layouts.admin');
     }
 }
--- resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -1,40 +1,102 @@
 
 <div class="right_col" role="main">
     <!-- top tiles -->
-    <div class="row tile_count">
-      <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-        <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
-        <div class="count">2500</div>
-        <span class="count_bottom"><i class="green">4% </i> From last Week</span>
+    <div class="row top_tiles" style="margin: 10px 0;">
+      
+      <div class="col-md-3 tile">
+        <span>Total Sales</span>
+        <h2>{{$totalSales}}</h2>
+        
       </div>
-      <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-        <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
-        <div class="count">123.50</div>
-        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
+      <div class="col-md-3 tile">
+        <span>Total Revenue</span>
+        <h2>₹ {{$totalRevenue}}</h2>
+        
       </div>
-      <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-        <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
-        <div class="count green">2,500</div>
-        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+      <div class="col-md-3 tile">
+        <span>Today Sale</span>
+        <h2>{{$todaySale}}</h2>
+        
       </div>
-      <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-        <span class="count_top"><i class="fa fa-user"></i> Total Females</span>
-        <div class="count">4,567</div>
-        <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>
-      </div>
-      <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-        <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
-        <div class="count">2,315</div>
-        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
-      </div>
-      <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-        <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
-        <div class="count">7,325</div>
-        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+      <div class="col-md-3 tile">
+        <span>Today Revenue</span>
+        <h2>₹ {{$todayRevenue}}</h2>
+        
       </div>
     </div>
     <!-- /top tiles -->
 
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="dashboard_graph">
+
+          <div class="row x_title">
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="panel panel-default">
+                      
+                      <div class="panel-heading">
+                          Latest  Orders
+                      </div>
+                      <div class="panel-body">
+                          <table class="table table-striped">
+                              <thead>
+                                  <tr>
+                                      <th>Id</th>
+                                      <th>Subtotal</th>
+                                      <th>Discount</th>
+                                      <th>Tax</th>
+                                      <th>Total</th>
+                                      <th>FirstNname</th>
+                                      <th>Last Name</th>
+                                      <th>Mobile</th>
+                                      <th>Email</th>
+                                      <th>Zipcode</th>
+                                      <th>Status</th>
+                                      <th>Order Date</th>
+                                      <th class="text-center">Actions</th>
+
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                  @foreach ($orders as $order)
+                                  <tr>
+                                      <td>{{$order->id}}</td>
+                                      <td>₹ {{$order->subtotal}}</td>
+                                      <td>₹ {{$order->discount}}</td>
+                                      <td>₹ {{$order->tax}}</td>
+                                      <td>₹ {{$order->total}}</td>
+                                      <td>{{$order->firstname}}</td>
+                                      <td>{{$order->lastname}}</td>
+                                      <td>{{$order->mobile}}</td>
+                                      <td>{{$order->email}}</td>
+                                      <td>{{$order->zipcode}}</td>
+                                      <td>{{$order->status}}</td>
+                                      <td>{{$order->created_at}}</td>
+                                      <td><a href="{{route('admin.orderdetails',['order_id' => $order->id])}}"
+                                              class="btn btn-info btn-sm">Details
+                                          </a>
+                                      </td>
+                                     
+
+                                  </tr>
+                                  @endforeach
+                              </tbody>
+                          </table>
+                          
+                      </div>
+                  </div>
+              </div>
+          </div>
+          </div>
+
+          
+
+          <div class="clearfix"></div>
+        </div>
+      </div>
+
+    </div>
     <div class="row">
       <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="dashboard_graph">
